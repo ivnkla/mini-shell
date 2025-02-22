ab7490808c47d91c25a1778e8a2f8ccc9dd1e5b0
Kalvin-Giang Pichon <kapi@mb-kap.local>
2025-02-22 15:19:51 +0100

    Gère un seul pipe mtn faut gérer plusieurs

--- src/shell.c
+++ src/shell.c
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include "readcmd.h"
 #include "csapp.h"
-#define MAXSIZE (1000)
 /*part3 et 4, simple command and redirections*/
 void cmd_simple(struct cmdline* l,char **cmd) {
 	pid_t pid = Fork();
@@ -45,45 +44,52 @@ void cmd_simple(struct cmdline* l,char **cmd) {
 }
 
 /* part 6*/
-void pipe_cmd_simple() {
+void pipe_cmd_simple(char **cmd1, char **cmd2) {
 	int fd[2]; //fd[0] = read, fd[1] = write
 	pid_t pid1, pid2;
 	if (pipe(fd) == -1) {
 		perror("Pipe error ");
 		exit(EXIT_FAILURE);
 	}
-	pid1 = Fork();
+	if ((pid1 = Fork())==-1) {
+		perror("Fork error ");
+		exit(EXIT_FAILURE);
+	}
+
 	//first command
 	if (pid1 == 0) {
 		Close(fd[0]); //close the pipe read
-		Dup2(fd[1], 1); //make stdout points to fd
+		Dup2(fd[1], STDOUT_FILENO); //make stdout points to fd
 		Close(fd[1]);
-		execlp("ls", "ls", "-l", NULL);
-		perror("execvp ls -l ");
+		execvp(cmd1[0], &cmd1[0]);
+		perror("execvp ");
 		exit(EXIT_FAILURE);
 
 	}
-	pid2 = Fork();
+	if ((pid2 = Fork())==-1) {
+		perror("Fork error ");
+		exit(EXIT_FAILURE);
+	}	
+	//second command
 	if (pid2 == 0) {
 		Close(fd[1]); //close the pipe write
-		Dup2(fd[0], 0); //makes stdin points to fd
+		Dup2(fd[0], STDIN_FILENO); //makes stdin points to fd
 		Close(fd[0]);
-		execlp("less", "less", NULL);
-		perror("execvp less ");
+		execvp(cmd2[0], &cmd2[0]);
+		perror("execvp ");
 		exit(EXIT_FAILURE);
 	}
 	//father
 	Close(fd[0]);
 	Close(fd[1]);
 	waitpid(pid1, NULL, 0);
-	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return;
 }
 
 int main()
 {
 
-	pipe_cmd_simple();
 	while (1) {
 		struct cmdline *l;
 		int i, j;//, k;
@@ -117,19 +123,26 @@ int main()
 		if (l->out) printf("out: %s\n", l->out);
 
 		/* Execute each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			//printf("seq[%d]: ", i);
-			/*for (j=0; cmd[j]!=0; j++) {
-				//printf("command of the pipe: \n");
-				//printf("cmd[%d]: %s ", j, cmd[j]);
-				//printf("command of the sequence: \n");
-				for (k=0; cmd[j][k]!=0; k++) {
-					printf("%s ", cmd[j][k]);
-				}
-			}*/
-			//printf("\n");
-			cmd_simple(l,cmd);
-		}
+		//for (i=0; l->seq[i]!=0; i++) {
+			i = 0;
+			char **cmd1 = l->seq[i];
+			if (l->seq[i+1]!=0) {
+				char **cmd2 = l->seq[i+1];
+				pipe_cmd_simple(cmd1, cmd2);
+			} else {
+				cmd_simple(l, cmd1);
+			}
+			/*printf("seq[%d]: ", i);
+			for (j=0; cmd[j]!=0; j++) {
+				printf("command of the pipe: \n");
+				printf("cmd[%d]: %s ", j, cmd[j]);
+				printf("command of the sequence: \n");
+				//for (k=0; cmd[j][k]!=0; k++) {
+				//	printf("%s ", cmd[j][k]);
+				//}
+			}
+			printf("\n");
+			//cmd_simple(l,cmd);*/
+		//}
 	}
 }
