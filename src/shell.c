26ae7fd648f748e2d47040f0ae4697a56729a4bc
Kalvin-Giang Pichon <kapi@mb-kap.local>
2025-02-23 12:59:11 +0100

    multiples pipes and redicrectiosn handled in one function

--- src/shell.c
+++ src/shell.c
@@ -2,12 +2,13 @@
  * Copyright (C) 2002, Simon Nieuviarts
  */
 
- /* You'll find a detailed explanation of the functions in the repo https://github.com/ivnkla/mini-shell in the EREADME file*/
+ /* NOTE : You'll find a detailed explanation of the functions in the repo https://github.com/ivnkla/mini-shell in the EREADME file*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "readcmd.h"
 #include "csapp.h"
+#include <stdbool.h>
 
 //adding a debug option, pas sûr de l'utilité...
 #ifdef DEBUG
@@ -16,7 +17,7 @@
     #define DEBUG_PRINT(fmt, args...)
 #endif
 
-
+/*USELESS CAUSE HANDLED IN THE PIPELINE METHOD*/
 /*part3 et 4, simple command and redirections*/
 void cmd_simple(struct cmdline* l,char **cmd) {
 	pid_t pid;
@@ -62,83 +63,79 @@ void cmd_simple(struct cmdline* l,char **cmd) {
 	}
 }
 
-/* part 6 one pipe with (or without) redirection */
-void pipe_cmd_simple(struct cmdline* l, char **cmd1, char **cmd2) {
-    int fd[2];
-    pid_t pid1, pid2;
-    int in_fd, out_fd = -1;
-    
-    if (pipe(fd) == -1) {
-        perror("Pipe error ");
-        exit(EXIT_FAILURE);
-    }
-    
-    if ((pid1 = Fork()) == -1) {
-        perror("Fork error ");
-        exit(EXIT_FAILURE);
-    }
- 
- 	//handling cmd1 with first child
-    if (pid1 == 0) {
-        //if input redirection, make point stdin to that input file 
-        if (l->in) {
-            in_fd = Open(l->in, O_RDONLY, 0444);
-            if (in_fd == -1) {
-                perror("Open input file error");
-                exit(EXIT_FAILURE);
-            }
-            Dup2(in_fd, STDIN_FILENO);
-            Close(in_fd);
+//just do a single cmd and read an input (or write an ouput) if need, we keep it simple here
+void pipe_cmd_simple(struct cmdline* l, char **cmd, int read_fd, int write_fd) {
+    pid_t pid;
+
+    if ((pid = Fork()) == 0) {
+        if (read_fd != -1) {
+            Dup2(read_fd, STDIN_FILENO); //make point STDIN into read_fd
+            Close(read_fd);
         }
-        
-        //and then, in any case, write the result into the pipe
-        Dup2(fd[1], STDOUT_FILENO);//making point stdout into the pipe
-        Close(fd[0]);
-        Close(fd[1]);
-        execvp(cmd1[0], cmd1);
-        perror("execvp error cmd1");
+        if (write_fd != -1) {
+            Dup2(write_fd, STDOUT_FILENO); //make point STDOUT into write_fd
+            Close(write_fd);
+        }
+        execvp(cmd[0], cmd);
+        perror("execvp error");
         exit(EXIT_FAILURE);
+    }
+	waitpid(pid, NULL, 0);
+}
+
+//manage the whole pipeline here
+void execute_pipeline(struct cmdline* l) {
+    int fd[2];
+    int prev_read_fd = -1;
+	int last_write_fd = -1;
+
+	//dealing with input and output files respectively only for the first and last command
+	if (l->in) {
+		//we attribute the fildes to the prev_read_fd
+		if ((prev_read_fd = Open(l->in, O_RDONLY, 0444)) == -1) {
+			perror("in opening error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (l->out) {
+		if ((last_write_fd = Open(l->out, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
+			perror("out opening error");
+			exit(EXIT_FAILURE);
+ 		}
+	}
+
+    int i = 0;
+    while (l->seq[i] != NULL) {
+        if (l->seq[i + 1] != NULL) { //if there is following commands
+            pipe(fd); //create a pipe for that pair
+            pipe_cmd_simple(l, l->seq[i], prev_read_fd, fd[1]); //write the output of cmd1 into the pipe taking prev_read_fd as an input
+            Close(fd[1]);
+            if (prev_read_fd != -1) {
+				Close(prev_read_fd);
+			}
+            prev_read_fd = fd[0]; //keeping that new read for the next command
+        } else { //last command
+            pipe_cmd_simple(l, l->seq[i], prev_read_fd, last_write_fd);
+            if (prev_read_fd != -1) {
+				Close(prev_read_fd);
+			}
+			if (last_write_fd != -1) {
+				Close(last_write_fd);
+			}
+        }
+        i++;
     }
 	
-	//by the same way, handling cm2 with second child
-    if ((pid2 = Fork()) == -1) {
-        perror("Fork error ");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid2 == 0) {
-		//we immediatly make point stdin to the pipe, child2 has to read the result from cmd1
-        Dup2(fd[0], STDIN_FILENO);
-        //treat the case of an 'output' redirection, similar than before
-        if (l->out) {
-            out_fd = Open(l->out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (out_fd == -1) {
-                perror("Open output file error");
-                exit(EXIT_FAILURE);
-            }
-            Dup2(out_fd, STDOUT_FILENO);
-            Close(out_fd);
-        }
-        
-        Close(fd[0]);
-        Close(fd[1]);
-        execvp(cmd2[0], cmd2);
-        perror("execvp error cmd2");
-        exit(EXIT_FAILURE);
-    }
-
-    //finish the process from the parent
-    Close(fd[0]);
-    Close(fd[1]);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
+	//dirty way to do that buttt, idk if even usefull
+    //while (wait(NULL) > 0);
 }
+
+
 int main()
 {
 
 	while (1) {
 		struct cmdline *l;
-		int i;// j;//, k;
 
 		printf("shell> ");
 		l = readcmd();
@@ -169,26 +166,6 @@ int main()
 		if (l->out) DEBUG_PRINT("out: %s", l->out);
 
 		/* Execute each command of the pipe */
-		//for (i=0; l->seq[i]!=0; i++) {
-			i = 0;
-			char **cmd1 = l->seq[i];
-			if (l->seq[i+1]!=0) {
-				char **cmd2 = l->seq[i+1];
-				pipe_cmd_simple(l,cmd1, cmd2);
-			} else {
-				cmd_simple(l, cmd1);
-			}
-			/*printf("seq[%d]: ", i);
-			for (j=0; cmd[j]!=0; j++) {
-				printf("command of the pipe: \n");
-				printf("cmd[%d]: %s ", j, cmd[j]);
-				printf("command of the sequence: \n");
-				//for (k=0; cmd[j][k]!=0; k++) {
-				//	printf("%s ", cmd[j][k]);
-				//}
-			}
-			printf("\n");
-			//cmd_simple(l,cmd);*/
-		//}
+		execute_pipeline(l);
 	}
 }
